09b0bf934d5d131a69e8364dfe9a6890ac056366
Сергей Цыбренок <cybrenok91@mail.ru>
2024-11-05 20:49:41 +0300

    memory3

--- neyral_game.py
+++ neyral_game.py
@@ -1,19 +1,20 @@
 import os
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # Убирает информационные и предупреждающие сообщения от TensorFlow
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 
 import pandas as pd
 import tensorflow as tf
 
 tf.keras.utils.disable_interactive_logging()
+
 import numpy as np
 from random import randint
 
 
-def get_model():
-    if os.path.exists('guessing_model.keras'):
-        model = tf.keras.models.load_model('guessing_model.keras')
+def get_model(name: str, path: str):
+    if os.path.exists(path):
+        model = tf.keras.models.load_model(name)
         print("Модель загружена.")
     else:
         model = tf.keras.Sequential([
@@ -28,16 +29,17 @@ def get_model():
     return model
 
 
-def get_model_with_memory():
+def get_model_with_memory(name: str, path: str):
     input_shape = (sequence_length, 2)
-    if os.path.exists('memory_model.keras'):
-        model = tf.keras.models.load_model('guessing_model.keras')
+    if os.path.exists(path):
+        model = tf.keras.models.load_model(name)
         print("Модель загружена.")
     else:
         model = tf.keras.Sequential([
             tf.keras.layers.Input(shape=input_shape),
-            tf.keras.LSTM(32, activation='relu', return_sequences=False),
-            tf.keras.layers.Dense(16, activation='relu'),
+            tf.keras.layers.LSTM(128, activation='relu', return_sequences=False),
+            tf.keras.layers.Dense(64, activation='relu'),
+            tf.keras.layers.Dense(32, activation='relu'),
             tf.keras.layers.Dense(1)
         ])
         print("Создана новая модель.")
@@ -48,12 +50,8 @@ def get_model_with_memory():
 
 def neural_network_guess(model):
     target = randint(1, 100)
-    guess = np.array([[50]])  # Начальное предположение
+    guess = np.array([[50]])
     attempts = 0
-    history_lenght: int = 5
-    history: list = []
-
-    # print("Компьютер загадал число от 1 до 100.")
 
     while True:
         prediction = model.predict(guess)[0][0]
@@ -98,73 +96,64 @@ def get_repeat_count():
             print("Пожалуйста, вводите целое число")
 
 
-sequence_length: int = 3
+sequence_length: int = 30
 
 
-def neural_network_guess_with_memory(model, target):
-    # Начальные параметры
-    history = []  # История попыток
+def neural_network_guess_with_memory(model, target, log: bool):
+    history = []
 
-    for attempt in range(1, 101):  # Максимум 100 попыток
-        # Формируем входные данные из истории
+    for attempt in range(1, 101):
         if len(history) < sequence_length:
             padded_history = [[0, 0]] * (sequence_length - len(history)) + history
         else:
             padded_history = history[-sequence_length:]
 
-        # Преобразуем в формат np.array для модели
         input_sequence = np.array([padded_history])
 
-        # Получаем предсказание от модели
         prediction = model.predict(input_sequence, verbose=0)[0][0]
         guess = round(prediction)
-        print(f"Нейросеть предполагает: {guess}", end=" → ")
+        if log: print(f"Нейросеть предполагает: {guess}", end=" →\n")
 
-        # Проверяем результат угадывания
         if guess == target:
             print("Угадала!")
-            return
+            return [attempt, target]
         elif guess < target:
             feedback = 1
-            # print("больше!")
         else:
             feedback = -1
-            # print("меньше!")
 
-        # Добавляем текущую попытку в историю
         history.append([guess, feedback])
 
-        # Обучаем модель на текущей попытке
         target_value = target if feedback == 1 else target - 1
-        model.fit(input_sequence, np.array([[target_value]]), epochs=1, verbose=0)
+        model.fit(input_sequence, np.array([[target_value]]), epochs=5, verbose=0)
 
     print("Не удалось угадать число за 100 попыток.")
-
+    return [100, target]
 
 def main():
+    model_info: dict = {"name": "my_memory_model_test4.keras", "path": "my_memory_model4_test.keras"}
     repeat_count: int = get_repeat_count()
-    my_model = get_model_with_memory()
+    my_model = get_model_with_memory(model_info["name"], model_info["path"])
 
+    attempts: list = []
+    count = 0
+    while count != repeat_count:
+        target_value: int = randint(1, 100)
+        list_attempt: list = neural_network_guess_with_memory(my_model, target_value, False)
+        attempts.append(list_attempt)
+        count += 1
 
-    # attempts: list = []
-    # count = 0
-    # while count != repeat_count:
-    #     list_attempt: int = ney_model)
-    #     attempts.append(list_attempt)
-    #     count += 1
-
+    saveModel(my_model, model_info["name"])
     # average: float = sum(attempts) / len(attempts)
     # print(f"Среднее количество попыток: {average:.1f}")
     # print(f"Max: {max(attempts)}\nMin: {min(attempts)}")
 
-    saveModel(my_model)
     data_frame = pd.DataFrame(attempts)
     try:
-        print(data_frame)
+        print("Table save!")
     except:
-        # notink
-        print()
-    data_frame.to_csv("table.csv", index=False, header=False)
+        print("Error")
+    data_frame.to_csv("table_memory6.csv", index=False, header=False)
 
 
 main()
